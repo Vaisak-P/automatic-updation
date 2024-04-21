d1678d731e23497438bf3050de543feed386ad70
Vaisak-P <vaisakp530@gmail.com>
2024-04-21 15:42:00 +0300

    completed autoupdate feature

--- index.js
+++ index.js
@@ -8,9 +8,46 @@ autoUpdater.autoInstallOnAppQuit = false;
 autoUpdater.on("checking-for-update", () => {
   dialog.showMessageBox({message: "checking-for-update"})
 })
-autoUpdater.on("update-available", () => {
-  dialog.showMessageBox({message: "update-available"})
-})
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    message: 'An update is available. Do you want to download and install it?',
+    buttons: ['Yes', 'No'],
+    defaultId: 0, // Index of the default button (Yes)
+    cancelId: 1, // Index of the cancel button (No)
+  }).then((response) => {
+    if (response.response === 0) {
+      autoUpdater.downloadUpdate();
+    } else {
+      // User clicked 'No', do something else or inform the user
+    }
+  }).catch((err) => {
+    console.error('Error showing message box:', err);
+  });
+});
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    message: 'Update downloaded. Do you want to install it now?',
+    buttons: ['Install', 'Later'],
+    defaultId: 0, // Index of the default button (Install)
+    cancelId: 1, // Index of the cancel button (Later)
+  }).then((response) => {
+    if (response.response === 0) {
+      // User clicked 'Install', proceed with update installation
+      autoUpdater.quitAndInstall();
+    } else {
+      // User clicked 'Later', you can inform the user or take other actions
+    }
+  }).catch((err) => {
+    console.error('Error showing message box:', err);
+  });
+});
+
+
+
 autoUpdater.on("update-not-available", () => {
   dialog.showMessageBox({message: "update-not-available"})
 })
